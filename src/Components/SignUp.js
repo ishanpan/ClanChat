d5ead60b333426c964099ebdfb1407282a31724c
ishanpan <70710170+ishanpan@users.noreply.github.com>
2021-09-15 12:49:00 +0530

    CSS

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -4,75 +4,70 @@ import * as Yup from "yup";
 import styles from "./SignUp.module.css";
 
 const SignUp = () => {
-	const formik = useFormik({
-		initialValues: {
-			firstName: "",
-			lastName: "",
-			email: "",
-		},
-		validationSchema: Yup.object({
-			firstName: Yup.string()
-				.max(15, "Must be 15 characters or less")
-				.required("Required"),
-			lastName: Yup.string()
-				.max(20, "Must be 20 characters or less")
-				.required("Required"),
-			email: Yup.string().email("Invalid email address").required("Required"),
-		}),
-		onSubmit: (values) => {
-			console.log(values.firstName);
-		},
-	});
-	return (
-		<form className={styles.form} nSubmit={formik.handleSubmit}>
-			<label className={styles.formVal} htmlFor="firstName">
-				First Name
-			</label>
-			<input
-				id="firstName"
-				name="firstName"
-				type="text"
-				onChange={formik.handleChange}
-				onBlur={formik.handleBlur}
-				value={formik.values.firstName}
-			/>
-			{formik.touched.firstName && formik.errors.firstName ? (
-				<div>{formik.errors.firstName}</div>
-			) : null}
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      lastName: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+      email: Yup.string().email("Invalid email address").required("Required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values.firstName);
+    },
+  });
+  return (
+    <div className={styles.form}>
+      <form className={styles.inputs} nSubmit={formik.handleSubmit}>
+        <div className={styles.formheading}>Create An Account</div>
+        <div className={styles.formlabin}>
+          <input
+            className={styles.forminput}
+            id="firstName"
+            name="firstName"
+            type="text"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.firstName}
+            placeholder="John_Doe"
+          />
+          {formik.touched.firstName && formik.errors.firstName ? (
+            <div className={styles.formerr}>{formik.errors.firstName}</div>
+          ) : null}
+        </div>
 
-			<label className={styles.formVal} htmlFor="lastName">
-				Last Name
-			</label>
-			<input
-				id="lastName"
-				name="lastName"
-				type="text"
-				onChange={formik.handleChange}
-				onBlur={formik.handleBlur}
-				value={formik.values.lastName}
-			/>
-			{formik.touched.lastName && formik.errors.lastName ? (
-				<div>{formik.errors.lastName}</div>
-			) : null}
-
-			<label className={styles.formVal} htmlFor="email">
-				Email Address
-			</label>
-			<input
-				id="email"
-				name="email"
-				type="email"
-				onChange={formik.handleChange}
-				onBlur={formik.handleBlur}
-				value={formik.values.email}
-			/>
-			{formik.touched.email && formik.errors.email ? (
-				<div>{formik.errors.email}</div>
-			) : null}
-
-			<button type="submit">Submit</button>
-		</form>
-	);
+        <div className={styles.formlabin}>
+          <input
+            className={styles.forminput}
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+            placeholder="johndoe@xyz.com"
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <div className={styles.formerr}>{formik.errors.email}</div>
+          ) : null}
+        </div>
+        <div className={styles.formlabin}>
+          <div className={styles.submitbtn}>
+            <button className={styles.submitbtntext} type="submit">
+              Create Account
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
